51ed9ef478ff64a02cb44bf7351c3fcec7fabf40
Vansh Pali <vanshrpali905@gmail.com>
2024-09-08 23:26:13 +0530

    included dependencies

--- Assets/VScript.cs
+++ Assets/VScript.cs
@@ -6,6 +6,7 @@ public class VScript : MonoBehaviour
 {
     [SerializeField]
     private float Count;
+    public GameObject Object1;
 
     // Start is called before the first frame update
     void Start()
